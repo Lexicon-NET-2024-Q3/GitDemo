8a3ff4c70322b4099acc989d94520e5c9d419029
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-10-28 11:14:03 +0100

    Add rest

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -5,6 +5,10 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+
         Console.WriteLine("Hej");
     }
 }
